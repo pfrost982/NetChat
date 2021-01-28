b9a0611f07cf515aea1aef01ab8a5fdfce99be6a
pfrost982 <power982@gmail.com>
2021-01-28 17:47:20 +0700

    lesson7 start

---
+++ src/lesson7/client/ClientMain.java
@@ -0,0 +1,31 @@
+package lesson7.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ClientMain.class.getResource("clientChat.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Net chat");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        Network network = new Network();
+        network.connect();
+        ClientController clientController = loader.getController();
+        clientController.setNetwork(network);
+        network.waitMessage(clientController);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/lesson7/client/Network.java
@@ -0,0 +1,63 @@
+package lesson7.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Network {
+
+    private static final int DEFAULT_SERVER_SOCKET = 8888;
+    private static final String DEFAULT_SERVER_HOST = "localhost";
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private final int port;
+    private final String host;
+
+    public Network(String host, int port) {
+        this.port = port;
+        this.host = host;
+    }
+
+    public Network() {
+        this.host = DEFAULT_SERVER_HOST;
+        this.port = DEFAULT_SERVER_SOCKET;
+    }
+
+    public void connect() {
+        try {
+            socket = new Socket(host, port);
+
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Соединение не установлено");
+            System.exit(-1);
+            e.printStackTrace();
+        }
+    }
+
+    public DataOutputStream getOut() {
+        return out;
+    }
+
+    public void waitMessage(ClientController Controller) {
+        Thread thread = new Thread(() -> {
+            try {
+                while (true) {
+                    String message = in.readUTF();
+                    Controller.appendMessage("Сервер", message);
+                }
+            } catch (IOException e) {
+                System.out.println("Ошибка подключения");
+                System.exit(-1);
+            }
+
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+}
